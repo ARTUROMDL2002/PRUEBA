1e6339427a8862f02659c5f112a7447cb55708a0
Arturo <skipper.mendoza19@gmail.com>
2023-03-29 19:03:20 -0600

    inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClasicosComponent } from './clasicos/clasicos.component';
+import { MarcasComponent } from './marcas/marcas.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { LoginComponent } from './login/login.component';
+import { AutoresComponent } from './autores/autores.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ClasicosComponent,
+    MarcasComponent,
+    ContactoComponent,
+    LoginComponent,
+    AutoresComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/clasicos/clasicos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-clasicos',
+  templateUrl: './clasicos.component.html',
+  styleUrls: ['./clasicos.component.css']
+})
+export class ClasicosComponent {
+
+}
